c03bd8037592c4945b48b9b598e92e4dd4dee746
ajSeadler <aj6287@gmail.com>
2023-10-23 11:25:30 -0500

    update tables - forgot to make a branch sry

--- src/server/db/seed.js
+++ src/server/db/seed.js
@@ -1,85 +1,106 @@
-const db = require('./client');
-const { createUser } = require('./users');
+const db = require("./client");
+const { createUser } = require("./users");
 
 const users = [
   {
-    name: 'Emily Johnson',
-    email: 'emily@example.com',
-    password: 'securepass',
+    name: "Emily Johnson",
+    email: "emily@example.com",
+    password: "securepass",
   },
   {
-    name: 'Liu Wei',
-    email: 'liu@example.com',
-    password: 'strongpass',
+    name: "Liu Wei",
+    email: "liu@example.com",
+    password: "strongpass",
   },
   {
-    name: 'Isabella García',
-    email: 'bella@example.com',
-    password: 'pass1234',
+    name: "Isabella García",
+    email: "bella@example.com",
+    password: "pass1234",
   },
   {
-    name: 'Mohammed Ahmed',
-    email: 'mohammed@example.com',
-    password: 'mysecretpassword',
+    name: "Mohammed Ahmed",
+    email: "mohammed@example.com",
+    password: "mysecretpassword",
   },
   {
-    name: 'John Smith',
-    email: 'john@example.com',
-    password: 'password123',
+    name: "John Smith",
+    email: "john@example.com",
+    password: "password123",
   },
-  // Add more user objects as needed
-];  
+];
 
 const dropTables = async () => {
-    try {
-        await db.query(`
+  try {
+    await db.query(`
         DROP TABLE IF EXISTS users;
-        `)
-    }
-    catch(err) {
-        throw err;
-    }
-}
+        `);
+  } catch (err) {
+    throw err;
+  }
+};
+
+// users table has a primary key on the id field.
+// restaurants table has a primary key on the id field.
+// reviews table has a primary key on the id field.
+// The user_id field in the reviews table is a foreign key that references the id field in the users table, establishing a relationship between reviews and users.
+// The restaurant_id field in the reviews table is a foreign key that references the id field in the restaurants table, establishing a relationship between reviews and restaurants.
 
 const createTables = async () => {
-    try{
-        await db.query(`
+  try {
+    await db.query(`
         CREATE TABLE users(
             id SERIAL PRIMARY KEY,
             name VARCHAR(255) DEFAULT 'name',
             email VARCHAR(255) UNIQUE NOT NULL,
             password VARCHAR(255) NOT NULL
-        )`)
-    }
-    catch(err) {
-        throw err;
-    }
-}
+            is_admin BOOLEAN NOT NULL DEFAULT FALSE
+        )`);
+    await db.query(`
+        CREATE TABLE restaurants (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            address VARCHAR(255) NOT NULL,
+            phone_number VARCHAR(15)
+        )`);
+    await db.query(`
+        CREATE TABLE reviews (
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id),
+            restaurant_id INT REFERENCES restaurants(id),
+            rating INT NOT NULL,
+            review_text TEXT
+        )`);
+  } catch (err) {
+    throw err;
+  }
+};
 
 const insertUsers = async () => {
   try {
     for (const user of users) {
-      await createUser({name: user.name, email: user.email, password: user.password});
+      await createUser({
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      });
     }
-    console.log('Seed data inserted successfully.');
+    console.log("Seed data inserted successfully.");
   } catch (error) {
-    console.error('Error inserting seed data:', error);
+    console.error("Error inserting seed data:", error);
   }
 };
 
 const seedDatabse = async () => {
-    try {
-        db.connect();
-        await dropTables();
-        await createTables();
-        await insertUsers();
-    }
-    catch (err) {
-        throw err;
-    }
-    finally {
-        db.end()
-    }
-}
+  try {
+    db.connect();
+    await dropTables();
+    await createTables();
+    await insertUsers();
+  } catch (err) {
+    throw err;
+  } finally {
+    db.end();
+  }
+};
 
-seedDatabse()
+seedDatabse();
