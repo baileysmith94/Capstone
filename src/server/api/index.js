10c6efef2c0ecb3f0d78b61ae5ba1eff59aec318
baileyborrego <baileyborrego@gmail.com>
2023-10-24 10:09:17 -0600

    Api Index

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const apiRouter = express.Router();
-const jwt = require('jsonwebtoken');
 
+const jwt = require('jsonwebtoken');
+// const { JWT_SECRET } = process.env;
 const volleyball = require('volleyball')
 apiRouter.use(volleyball)
 
 // TO BE COMPLETED - set `req.user` if possible, using token sent in the request header
 apiRouter.use(async (req, res, next) => {
+  const prefix = 'Bearer ';
   const auth = req.header('Authorization');
   
   if (!auth) { 
     next();
   } 
-  else if (auth.startsWith('REPLACE_ME')) {
+  else if (auth.startsWith(prefix)) {
     // TODO - Get JUST the token out of 'auth'
-    const token = 'REPLACE_ME';
+    const token = auth.slice(prefix.length);
     
     try {
-      const parsedToken = 'REPLACE_ME';
+      const parsedToken = jwt.verify(token, JWT_SECRET);
       // TODO - Call 'jwt.verify()' to see if the token is valid. If it is, use it to get the user's 'id'. Look up the user with their 'id' and set 'req.user'
+      if (parsedToken) {
+        req.user = await getUserById(parsedToken);
+        next();
+      } else {
+        next({
+          name:'AuthorizationHeaderError',
+          message: 'Authorization token malformed',
+        });
+      }
 
     } catch (error) {
       next(error);
@@ -27,9 +38,17 @@ apiRouter.use(async (req, res, next) => {
   else {
     next({
       name: 'AuthorizationHeaderError',
-      message: `Authorization token must start with 'Bearer'`
+      message: `Authorization token must start with ${prefix}`
     });
   }
+});
+
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+    console.log('User is set:', req.user);
+  }
+
+  next();
 });
 
 const usersRouter = require('./users');
