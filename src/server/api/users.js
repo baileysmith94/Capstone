7bc00abbed51013763e2070c567a463ee34328fc
Cassandra Rockwell <cassielynn99@icloud.com>
2023-10-31 11:29:06 -0400

    specifying users to endpoint

--- src/server/api/users.js
+++ src/server/api/users.js
@@ -10,7 +10,7 @@ const {
 
 const jwt = require('jsonwebtoken')
 
-usersRouter.get('/', async( req, res, next) => {
+usersRouter.get('/users', async( req, res, next) => {
     try {
         const users = await getAllUsers();
 
