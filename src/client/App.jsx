eb2327d8ddf477ecbfc16fe35bc2b5faff2a15b9
Cassandra Rockwell <cassielynn99@icloud.com>
2023-10-31 11:18:47 -0400

    making sign up routes match

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -19,7 +19,7 @@ function App() {
         {/* <Route path = '/' element={<ReviewList />} /> */}
         {/* <Route path = '/' element={<Profile />} /> */}
       </Routes>
-        <img id='comp-img' src='./computer.png'></img>
+        {/* <img id='comp-img' src='./computer.png'></img> */}
     </div>
   );
 }
