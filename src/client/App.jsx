fff2c1a0e5793a29f4cfc1aa87c80f28d7621c97
ajSeadler <aj6287@gmail.com>
2023-10-31 10:25:15 -0500

    update nav bar

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -4,18 +4,22 @@ import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import RestaurantList from "./components/RestaurantList";
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import "./style.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <div className="navbar"> </div>
+      <NavBar />
       <Routes>
         {/* <Route path = '/' element={<Home />} /> */}
         <Route path="/" element={<Login />} />
-        <Route path="/" element={<SignUp />} />
-        <Route path="/" element={<RestaurantList />} />
-        <Route path="/" element={<UserList />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/restaurants" element={<RestaurantList />} />
+        {/* <Route path = '/' element={<UserList />} /> */}
         {/* <Route path = '/' element={<ReviewList />} /> */}
         {/* <Route path = '/' element={<Profile />} /> */}
       </Routes>
