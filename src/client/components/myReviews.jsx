9ba7b202b9a070b5d27e0b3fc99c4dd53e0ac151
baileyborrego <baileyborrego@gmail.com>
2023-11-02 10:35:13 -0600

    updates to my reviews and api users file

--- src/client/components/myReviews.jsx
+++ src/client/components/myReviews.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import { createReview } from ".../api/reviews.js"
 
-export default function CreateReviewForm(SHOULD ANYTHING BE PASSED IN HERE) {
+export default function CreateReviewForm(token) {
     const [user_id, setUser_id] = useState("");
     const [restaurant_id, setRestaurant_id] = useState("");
     const [rating, setRating] = useState("");
@@ -9,33 +9,53 @@ export default function CreateReviewForm(SHOULD ANYTHING BE PASSED IN HERE) {
     const [image_url, setImage_url]= useState(null);
     const [error, setError] = useState(null);
     const [noSuccess, setNoSuccess] =useState("");
+    const [review, setReview] = useState([]);
+
+    const createReview = async()=>{
+      try{
+        const response = await fetch(`http://localhost:3000/api/users/create_review`, {
+            method: "POST", 
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({
+              review: {
+                rating: rating, 
+                review_text: review_text, 
+                image_url: image_url
+              }
+            })
+        });
+        const result = await response.json();
+        if(result.success) {
+          // how to set user id
+          // setUser_id(${user_id});
+          setReview(data.review);
+          setRating("");
+          setReview_text("");
+          setImage_url(null);
+        }
+        return result;
+      }catch (error) {
+        setError(result.error.message);
+        setNoSuccess("You must log in to leave a review")
+      }
+    };
 
     async function handleSubmit(e) {
         e.preventDefault();
-        // is the (user_id, restaurant_id, rating, review_text, image_url)correct?
-        const APIData = await createReview(user_id, restaurant_id, rating, review_text, image_url);
-        if(APIData.success) {
-            // need help here!
-            const reviews = [];
-
-            setRating("");
-            setReview_text("");
-            setImage_url(null);
-        } else {
-            setError(APIData.error.message);
-            setNoSuccess("You must log in to leave a review")
-        }
+        createReview();
     }
 
     return (
-
       // code a drop down menu to pick from restaurants
+      // set restaurant id as target id
         <>
         <h2>New Review</h2>
         <form onSubmit={handleSubmit}>
           {error && <p>{error}</p>}
           {noSuccess && <p>{noSuccess}</p>}
-          {/* how to pick a restaurant to review? */}
           <input
             value={rating}
             type="int"
@@ -59,7 +79,22 @@ export default function CreateReviewForm(SHOULD ANYTHING BE PASSED IN HERE) {
           /><br/>
           <button>Submit</button>
         </form>
+        <div>
+              <h4>Review for {restaurant_id.name}</h4>
+              {review !== undefined && review.length > 0 ? (
+                <ul>
+                  {review.map((review) => (
+                    <li key={review.id}>
+                      <p>Rating: {review.rating}</p>
+                      <p>{review.review_text}</p>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>Loading reviews...</p>
+              )}
+            </div>
         </>
       );
 
-}
+};
