246cf41f5d0046d88fc14b32c8c1fa00b5c28624
ajSeadler <aj6287@gmail.com>
2023-11-11 11:32:55 -0600

    stlying, route to profile after log in

--- src/client/components/ProfilePage.jsx
+++ src/client/components/ProfilePage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap styles
 
 const ProfilePage = () => {
   const [userData, setUserData] = useState();
@@ -10,7 +11,7 @@ const ProfilePage = () => {
 
         const response = await fetch('http://localhost:3000/api/users/me', {
           headers: {
-            'Content-Type' : 'application/json',
+            'Content-Type': 'application/json',
             Authorization: `Bearer ${token}`,
           },
         });
@@ -19,26 +20,44 @@ const ProfilePage = () => {
           const data = await response.json();
           setUserData(data);
         } else {
-          
+          // Handle error
         }
       } catch (error) {
-        console.error('Error fetching user data:');
+        console.error('Error fetching user data:', error);
       }
     };
 
     fetchUserData();
-  }, []); 
+  }, []);
 
   return (
-    <div className='profile-page'>
-      <h1>Welcome</h1>
-      {userData && (
-        <div>
-          <p>Name: {userData.name}</p>
-          <p>Email: {userData.email}</p>
-          {/* Add other user data fields as needed */}
+    <div className='profile-page container mt-5'>
+      <div className='card'>
+        <div className='card-body'>
+          <h1 className='card-title mb-4'>Welcome, {userData?.name}!</h1>
+          {userData && (
+            <div>
+              <p className='card-text'>
+                <strong>Name:</strong> {userData.name}
+              </p>
+              <p className='card-text'>
+                <strong>Email:</strong> {userData.email}
+              </p>
+              {/* Add other user data fields as needed */}
+            </div>
+          )}
         </div>
-      )}
+      </div>
+
+      {/* Reviews Card */}
+      <div className='card mt-4'>
+        <div className='card-body'>
+          <h5 className='card-title mb-3'>Reviews</h5>
+          <p className='card-text'>
+            The developers are working on displaying your reviews. Please be patient, fool!
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
