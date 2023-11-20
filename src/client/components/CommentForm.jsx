06e5cfe5e1fc66bf254c0313bf1b706809ce4823
Sam B <maisterburns@gmail.com>
2023-11-20 11:50:58 -0600

    added commentbox to CommentForm

--- src/client/components/CommentForm.jsx
+++ src/client/components/CommentForm.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
+import CommentBox from "./CommentList";
 
 export default function CommentForm(reviewId) {
   const token = localStorage.getItem("token");
   const [comment, setComment] = useState("");
-  const review_id = reviewId.reviewId.reviewId
+  const review_id = reviewId.reviewId
   const [userData, setUserData] = useState({})
-  
-  console.log("Token", token);
-  console.log("user data", userData);
+  console.log("REVIEW ID", review_id)
+  // console.log("Token", token);
+  // console.log("user data", userData);
+  useEffect(()=>{
+    fetchUser();
+  }, [])
+
   async function fetchUser() {
     try {
       
@@ -20,40 +25,36 @@ export default function CommentForm(reviewId) {
         });
         if (userDataResponse.ok){
           const data = await userDataResponse.json();
+          // console.log("User Data:", data)
           setUserData(data)
         } else {
-          console.log(error, "Failed to fetch used data")
+          console.log(error, "Failed to fetch user data")
         }
       } catch (error) {
           console.log(error)
         }};
 
-    useEffect(()=>{
-      fetchUser();
-    }, [])
-
-  async function handleSubmit(e) {
-    e.preventDefault();
     
-    try {
-      
+  async function handleSubmit(e) {
+    // e.preventDefault();    
+    try {      
       const user_id = userData.id;
+      // console.log("User ID", user_id)
       const response = await fetch(`http://localhost:3000/api/comments`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify({
-          
+        body: JSON.stringify({          
            user_id,
-             review_id,
-            comment,
-          
+            review_id,
+            comment,          
         }),
       });
       const data = await response.json();
       if (response.ok) {
+        
         return data;
       } else {
         console.log("failed to fetch", response.status);
@@ -62,18 +63,19 @@ export default function CommentForm(reviewId) {
       console.log(error);
     }
   }
-
+  
   return (
-    <>
+    <><CommentBox reviewId={reviewId}/>
       <form onSubmit={handleSubmit}>
-        <label>
-          {" "}
+
           <input
-            value={comment}
+            value={comment} required
+            type="text"
+            name="comment_text"
             placeholder="Write a Comment..."
             onChange={(e) => setComment(e.target.value)}
           />
-        </label>
+        
 
         <button type="submit">Post Comment</button>
       </form>
