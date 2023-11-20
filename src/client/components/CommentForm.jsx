dc1649d3fd5a5e786d45ddccde6820722a77501e
Sam B <maisterburns@gmail.com>
2023-11-19 23:17:13 -0600

    restored userdata fetch in comments posting

--- src/client/components/CommentForm.jsx
+++ src/client/components/CommentForm.jsx
@@ -1,19 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function CommentForm(reviewId) {
+  const token = localStorage.getItem("token");
   const [comment, setComment] = useState("");
   const review_id = reviewId.reviewId.reviewId
+  const [userData, setUserData] = useState({})
   
+  console.log("Token", token);
+  console.log("user data", userData);
+  async function fetchUser() {
+    try {
+      
+      const userDataResponse = await fetch("http://localhost:3000/api/users/me", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        
+        });
+        if (userDataResponse.ok){
+          const data = await userDataResponse.json();
+          setUserData(data)
+        } else {
+          console.log(error, "Failed to fetch used data")
+        }
+      } catch (error) {
+          console.log(error)
+        }};
+
+    useEffect(()=>{
+      fetchUser();
+    }, [])
 
   async function handleSubmit(e) {
     e.preventDefault();
-    const token = localStorage.getItem("token");
+    
     try {
       
-      const user_id = 1;
-      console.log(review_id);
-      console.log(user_id);
-      console.log(comment);
+      const user_id = userData.id;
       const response = await fetch(`http://localhost:3000/api/comments`, {
         method: "POST",
         headers: {
